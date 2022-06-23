cae17bb76fec8d1d3575248fc82646fc63c89ba9
justicemerrick <justicemer@gmail.com>
2022-06-23 15:35:28 -0700

    readme

---
+++ 0x01-shell_permissions/README.d
@@ -0,0 +1,5 @@
+#Task 0. a script that switches the current user to the user betty (my name is betty)
+#Task 1. a script that prints the effective username of the current user (who am i)
+#Task 2. a script that prints all the groups the current user is part of (groups)
+#Task 3. a script that changes the owner of the file hello to the user betty (new owner)
+
